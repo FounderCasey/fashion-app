7ca76e29fa90655974858ff00796168156d0c057
FounderCasey <caseywilcox@outlook.com>
2018-07-29 09:18:56 -0700

    user pages

---
+++ src/components/Ambassador.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<div class="hero">
+			<div class="card">
+			</div>
+		</div>
+		<div class="container">
+    </div>
+	</div>
+</template>
+
+<script>
+  
+	export default {
+		name: 'ambassador',
+		data () {
+			return {
+			}
+		}
+	}
+</script>
+
+<style scoped="true">
+  
+	.hero {
+		height: 300px;
+		width: 100%;
+		background-color: #8179B7;
+		position: relative;
+	}
+	
+	.container {
+    width: 75%;
+    margin: auto;
+  }
+  
+</style>
